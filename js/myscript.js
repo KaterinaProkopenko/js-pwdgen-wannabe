fb797ecd66f7b22456c37b8f84f0d112d75acc27
KaterinaProkopenko <ekaprokopenko97@gmail.com>
2022-02-21 16:16:49 +0100

    L'esercizio è finito

--- js/myscript.js
+++ js/myscript.js
@@ -1,6 +1,13 @@
 const firstName = prompt("Qual è il tuo nome?");
 console.log(firstName);
-const secondName = prompt ("Qual è il tuo cognome?");
+
+const secondName = prompt("Qual è il tuo cognome?");
 console.log(secondName);
-const favoriteColor = prompt ("Qual è il tuo colore preferito?");
+
+const favoriteColor = prompt("Qual è il tuo colore preferito?");
 console.log(favoriteColor);
+
+const password = firstName + secondName + favoriteColor + 21;
+console.log(password);
+
+document.getElementById('password').innerHTML = password;
