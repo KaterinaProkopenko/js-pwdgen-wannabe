6bcf6ab8eb0076fdf1ced7c9ffb0f3570563a778
KaterinaProkopenko <ekaprokopenko97@gmail.com>
2022-02-21 15:47:33 +0100

    Aggiornamento (le domande)

---
+++ js/myscript.js
@@ -0,0 +1,6 @@
+const firstName = prompt("Qual è il tuo nome?");
+console.log(firstName);
+const secondName = prompt ("Qual è il tuo cognome?");
+console.log(secondName);
+const favoriteColor = prompt ("Qual è il tuo colore preferito?");
+console.log(favoriteColor);
